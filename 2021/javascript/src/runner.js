da30824ad4b1c002d44ab3b8cfc65832cefe79fc
John-Magne Bredal <john-magne.bredal@hemit.no>
2021-12-01 21:54:22 +0100

    Move testrun to runner

--- 2021/javascript/src/runner.js
+++ 2021/javascript/src/runner.js
@@ -4,6 +4,9 @@ import { readLines } from './common.js';
   import(`./${day}/solver.js`).then((obj) => {
     const { solve, solve2 } = obj;
     const data = readLines(`./src/${day}/input.txt`);
+    const testdata = readLines(`./src/${day}/testdata`);
+    console.log(`${day} test 1:`, 7, solve(testdata));
+    console.log(`${day} test 2:`, 5, solve2(testdata));
     console.log(`${day} solution 1:`, solve(data));
     console.log(`${day} solution 2:`, solve2(data));
   });
