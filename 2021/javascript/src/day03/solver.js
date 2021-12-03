7e3ad5306414611b99c59852783fdc661375a819
John-Magne Bredal <john-magne.bredal@hemit.no>
2021-12-03 15:13:55 +0100

    Solve day3

--- 2021/javascript/src/day03/solver.js
+++ 2021/javascript/src/day03/solver.js
@@ -1,5 +1,48 @@
 export function solve(data) {
-  const bitArrays = data.reduce((bitArrays, line) => {
+  const bitArrays = createBitArrays(data);
+  const mostCommonBitsArray = getMostCommonBits(bitArrays);
+  const leastBitArray = xor(mostCommonBitsArray);
+
+  const gammaRate = parseInt(mostCommonBitsArray.join(''), 2);
+  const epsilonRate = parseInt(leastBitArray.join(''), 2);
+
+  const powerConsumption = gammaRate * epsilonRate;
+  return powerConsumption;
+}
+
+
+export function solve2(data) {
+  const oxygenGeneratorRating = parseInt(getRemainingBitstring(data, false), 2);
+  const co2ScrubberRating = parseInt(getRemainingBitstring(data), 2);
+
+  return oxygenGeneratorRating * co2ScrubberRating;
+}
+
+function getRemainingBitstring(data, filterByLeastCommon=true) {
+  let lines = data.slice();
+  const bitSize = lines[0].length;
+  for (let i = 0; i < bitSize; i++) {
+    let commonBits = getMostCommonBits(createBitArrays(lines));
+    if (filterByLeastCommon) {
+      commonBits = xor(commonBits);
+    }
+
+    lines = lines.filter(line => +line[i] === commonBits[i]);
+    if (lines.length <= 1) {
+      break;
+    }
+  }
+
+  return lines[0];
+}
+
+function xor(array) {
+  return array.map(b => b === 1 ? 0 : 1);
+}
+
+function createBitArrays(data) {
+  // Create an array containing arrays with the vertical bits
+  return data.reduce((bitArrays, line) => {
     Array.from(line).forEach((digit, index) => {
       if (bitArrays[index]) {
         bitArrays[index].push(digit);
@@ -10,24 +53,12 @@ export function solve(data) {
 
     return bitArrays;
   }, []);
-
-  const mostCommonBitArray = getMostCommonBit(bitArrays);
-  const leastBitArray = mostCommonBitArray.map(b => b === 1 ? 0 : 1);
-
-  const gammaRate = parseInt(mostCommonBitArray.join(''), 2);
-  const epsilonRate = parseInt(leastBitArray.join(''), 2);
-
-  const powerConsumption = gammaRate * epsilonRate;
-  return powerConsumption;
 }
 
-function getMostCommonBit(bitArrays) {
+function getMostCommonBits(bitArrays) {
   return bitArrays.reduce((finalBitArray, bitArray, index) => {
     const oneCount = bitArray.filter(bit => bit === '1').length;
-    finalBitArray[index] = oneCount > bitArray.length - oneCount ? 1 : 0;
+    finalBitArray[index] = oneCount >= bitArray.length - oneCount ? 1 : 0;
     return finalBitArray;
   }, []);
-}
-
-export function solve2(data) {
 }
