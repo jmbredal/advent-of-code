70c415de6ab5c2f23382b348b1eed4e43f66a50c
John-Magne Bredal <john-magne.bredal@hemit.no>
2021-12-03 10:17:19 +0100

    Detect linebreakchar

--- 2021/javascript/src/common.js
+++ 2021/javascript/src/common.js
@@ -1,6 +1,21 @@
 import fs from 'fs';
 
 export function readLines(filename, removeEmptyLines=true) {
-  const lines = fs.readFileSync(filename).toString().split('\n');
+  const file = fs.readFileSync(filename).toString();
+  const splitChar = getLineBreakChar(file);
+  const lines = file.split(splitChar);
   return removeEmptyLines ? lines.filter(x => x) : lines;
+}
+function getLineBreakChar(string) {
+  const indexOfLF = string.indexOf('\n', 1);  // No need to check first-character
+
+  if (indexOfLF === -1) {
+      if (string.indexOf('\r') !== -1) return '\r';
+
+      return '\n';
+  }
+
+  if (string[indexOfLF - 1] === '\r') return '\r\n';
+
+  return '\n';
 }
