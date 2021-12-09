b2bd2313943aabd3986c16d414c3e31aa602927a
John-Magne Bredal <john-magne.bredal@hemit.no>
2021-12-09 08:33:22 +0100

    Solve task1 day09 js

---
+++ 2021/javascript/src/day09/solver.js
@@ -0,0 +1,48 @@
+import { readLines } from '../common.js';
+
+// solve('testdata');
+// solve('input');
+
+export function solve(filename) {
+  const lines = readLines(filename);
+  const lowPoints = findLowPoints(lines);
+  const riskLevels = lowPoints.map(p => +p + 1);
+  const sum = riskLevels.reduce((a, b) => a + b);
+  return sum;
+}
+
+export function solve2() {
+  return 'Not implemented'
+}
+
+function findLowPoints(lines) {
+  return lines
+    .map((line, lineNumber) => findLowpointsOnLine(line, lineNumber, lines))
+    .reduce((acc, curr) => acc.concat(curr), []);  // concat arrays
+}
+
+function findLowpointsOnLine(line, lineNumber, lines) {
+  const numbers = line.split('');
+  return numbers.filter((number, index) => {
+    const neighbours = [];
+    number = +number;
+
+    if (index > 0) {
+      neighbours.push(numbers[index - 1]);
+    }
+
+    if (index < numbers.length - 1) {
+      neighbours.push(numbers[index + 1]);
+    }
+
+    if (lineNumber > 0) {
+      neighbours.push(lines[lineNumber - 1][index]);
+    }
+
+    if (lineNumber < lines.length - 1) {
+      neighbours.push(lines[lineNumber + 1][index]);
+    }
+    const isLowest = neighbours.filter((n) => +n <= number).length === 0;
+    return isLowest;
+  });
+}
