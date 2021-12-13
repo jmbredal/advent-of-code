de35f8f24272386897531ec7b5c0b5c5a2d196a7
John-Magne Bredal <bredal@jmmi.no>
2021-12-13 19:55:09 +0100

    Refactor day13

--- 2021/javascript/src/day13/solver.js
+++ 2021/javascript/src/day13/solver.js
@@ -5,9 +5,7 @@ export function solve(filename) {
     const coordStrings = lines.filter(line => !isNaN(parseInt(line[0])));
     const folds = lines.filter(line => isNaN(parseInt(line[0])));
     const coords = coordStrings.map(c => c.split(',').map(Number));
-
-    const [direction, value] = folds[0].split(' ').reverse()[0].split('=');
-    const foldedCoords = fold(direction, value, coords);
+    const foldedCoords = fold(folds[0], coords);
     return new Set(foldedCoords.map(c => JSON.stringify(c))).size;
 }
 
@@ -15,57 +13,51 @@ export function solve2(filename) {
     const lines = readLines(filename);
     const coordStrings = lines.filter(line => !isNaN(parseInt(line[0])));
     const folds = lines.filter(line => isNaN(parseInt(line[0])));
-    let coords = coordStrings.map(c => c.split(',').map(Number));
-
-    for (const f of folds) {
-        const [direction, value] = f.split(' ').reverse()[0].split('=');
-        coords = fold(direction, value, coords);
-    }
-    printCoords(coords);
+    const startingCoords = coordStrings.map(c => c.split(',').map(Number));
+    const result = folds.reduce((coords, instruction) => fold(instruction, coords), startingCoords);
+    printCoords(result);
 
     return 'Look above';
 }
 
-function fold(direction, value, coords) {
-    const max = value * 2;
+function foldY(line, coords) {
+    const max = line * 2;
+    const topCoords = coords.filter(c => c[1] < line);
+    const bottomCoords = coords.filter(c => c[1] > line).map(c => {
+        const newY = max - c[1];
+        return [c[0], newY];
+    });
+    return topCoords.concat(bottomCoords);
+}
 
-    if (direction === 'y') {
-        const topCoords = coords.filter(c => c[1] < value);
-        const bottomCoords = coords.filter(c => c[1] > value).map(c => {
-            const newY = max - c[1];
-            return [c[0], newY];
-        });
+function foldX(line, coords) {
+    const max = line * 2;
+    const leftCoords = coords.filter(c => c[0] < line);
+    const rightCoords = coords.filter(c => c[0] > line).map(c => {
+        const newX = Math.abs(-max + c[0]);
+        return [newX, c[1]];
+    });
 
-        return topCoords.concat(bottomCoords);
+    return leftCoords.concat(rightCoords);
+}
+
+function fold(foldInstruction, coords) {
+    const [direction, value] = foldInstruction.split(' ').reverse()[0].split('=');
+    const folder = {
+        'y': foldY,
+        'x': foldX,
     }
-
-    if (direction === 'x') {
-        const leftCoords = coords.filter(c => c[0] < value);
-        const rightCoords = coords.filter(c => c[0] > value).map(c => {
-            const newX = Math.abs(-max + c[0]);
-            return [newX, c[1]];
-        });
-
-        return leftCoords.concat(rightCoords);
-    }
+    return folder[direction](value, coords);
 }
 
 function printCoords(coords) {
     const maxX = Math.max(...coords.map(c => c[0]));
     const maxY = Math.max(...coords.map(c => c[1]));
 
-    const map = [];
-    for (let y = 0; y <= maxY; y++) {
-        const row = Array(maxX + 1).fill(' ');
-        map.push(row);
-    }
-
-    for (const coord of coords) {
-        const [x, y] = coord;
-        map[y][x] = '#';
-    }
-
-    for (const row of map) {
-        console.log(row.join(''));
-    }
+    Array(maxY + 1).fill()
+        .map((_, index) => {
+            const markers = coords.filter(c => c[1] === index).map(c => c[0]);
+            return Array(maxX + 1).fill().map((_, index) => markers.includes(index) ? '#' : ' ').join('');
+        })
+        .forEach(line => console.log(line));
 }
