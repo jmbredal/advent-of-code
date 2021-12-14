55e79eb74ccf65367552e8e6cd55a661b50028d2
John-Magne Bredal <john-magne.bredal@hemit.no>
2021-12-14 20:02:26 +0100

    dilldall

--- 2021/javascript/src/day14/solver.js
+++ 2021/javascript/src/day14/solver.js
@@ -1,34 +1,31 @@
 import { readLines } from '../common.js';
 
-solve('input')
+solve('testdata')
 
 export function solve(filename) {
   const [startTemplate, rules] = parse(filename);
 
-  let template = startTemplate;
-  for (let index = 0; index < 10; index++) {
-    const pairs = createPairs(template);
-    let newTemplate = pairs.reduce((acc, pair) => {
-      acc.push(update(pair, rules));
-      return acc;
-    }, []).join('');
-    newTemplate = newTemplate + template[template.length - 1];
-    template = newTemplate;
+  const pairs = { 'NN': 1, 'NC': 1, 'CB': 1 };
+  for (let index = 0; index < 2; index++) {
+    updatePairs(pairs, rules);
   }
 
-  const mapping = template.split('').reduce((mapping, char) => {
-    if (mapping[char]) {
-      mapping[char]++;
-    } else {
-      mapping[char] = 1;
-    }
-    return mapping;
-  }, {});
+  console.log(pairs);
 
-  const sorted = Object.entries(mapping).sort((a, b) => a[1] - b[1]);
-  const min = sorted[0];
-  const max = sorted[sorted.length - 1];
-  console.log(max[1] - min[1]);
+
+  // const mapping = template.split('').reduce((mapping, char) => {
+  //   if (mapping[char]) {
+  //     mapping[char]++;
+  //   } else {
+  //     mapping[char] = 1;
+  //   }
+  //   return mapping;
+  // }, {});
+
+  // const sorted = Object.entries(mapping).sort((a, b) => a[1] - b[1]);
+  // const min = sorted[0];
+  // const max = sorted[sorted.length - 1];
+  // console.log(max[1] - min[1]);
 }
 
 export function solve2(filename) {
@@ -50,15 +47,22 @@ function parse(filename) {
   return [template, rules]
 }
 
-function createPairs(template) {
-  const pairs = [];
-  for (let index = 0; index < template.length - 1; index++) {
-    pairs.push(template.slice(index, index + 2));
+function updatePairs(pairMap, rules) {
+  console.log(pairMap);
+  const newMap = {};
+  for (const key in pairMap) {
+    const newPair = key[0] + rules[key];
+    if (newMap[newPair]) {
+      newMap[newPair]++;
+    } else {
+      newMap[newPair] = 1;
+    }
   }
-  return pairs;
+  return newMap;
 }
 
-function update(pair, rules) {
-  const [a, b] = pair.split('');
-  return [a, rules[pair]].join('');
+function* createPairs(template) {
+  for (let index = 0; index < template.length - 1; index++) {
+    yield template.slice(index, index + 2);
+  }
 }
