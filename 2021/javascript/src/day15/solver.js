96cbbd532a4c9680f5558c96caf6344b4afee087
John-Magne Bredal <john-magne.bredal@hemit.no>
2021-12-15 12:53:37 +0100

    solve task 2 day 15 js

--- 2021/javascript/src/day15/solver.js
+++ 2021/javascript/src/day15/solver.js
@@ -1,11 +1,4 @@
-import { readLines, getNeighbors, createMap } from '../common.js';
-
-// const solution = solve('input');
-// console.log(solution);
-
-function createCell(x, y, distance) {
-  return { x, y, distance };
-}
+import { readLines, getNeighbors, createMap, range } from '../common.js';
 
 export function solve(filename) {
   const lines = readLines(filename);
@@ -23,8 +16,54 @@ export function solve(filename) {
   return distanceMap[distanceMap.length - 1][distanceMap[0].length - 1];
 }
 
-export function solve2() {
+export function solve2(filename) {
+  const lines = readLines(filename);
+  const valueMap = createBigMap(lines);
+  const distanceMap = valueMap.map(line => {
+    return Array.from(line).fill(999999999);
+  })
 
+  distanceMap[0][0] = 0;
+  let coords = [createCell(0, 0, 0)];
+  while (coords.length > 0) {
+    step(coords, valueMap, distanceMap);
+  }
+
+  return distanceMap[distanceMap.length - 1][distanceMap[0].length - 1];
+}
+
+function createCell(x, y, distance) {
+  return { x, y, distance };
+}
+
+function createBigMap(lines) {
+  const map = duplicateHorizontally(lines, 4);
+  return duplicateVertically(map, 4);
+}
+
+function duplicateHorizontally(map, times) {
+  return map.map(line => {
+    const numbers = line.split('').map(Number);
+    return range(times - 1).reduce((acc, curr) => {
+      const duplicate = addToLine(numbers, curr);
+      return acc.concat(duplicate);
+    }, []);
+  });
+}
+
+function duplicateVertically(map, times) {
+  return range(times).reduce((acc, index) => {
+    const duplicate = map.map(line => addToLine(line, index + 1));
+    return acc.concat(duplicate);
+  }, [...map]);
+}
+
+// Create new array with values incremented by value, max 9
+function addToLine(numbers, value) {
+  return numbers.map(n => {
+    const newValue = n + value;
+    return Math.floor(newValue / 10) > 0 ? (newValue % 10) + 1 : newValue;
+  });
 }
 
 function step(unvisited, valueMap, distanceMap) {
