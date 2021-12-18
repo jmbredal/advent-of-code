f0050225981bcb9a35cf5680ca47c0b8d30c8ddd
John-Magne Bredal <bredal@jmmi.no>
2021-12-18 23:16:54 +0100

    Solve task 2 day 5 c#

--- 2021/dotnet/day05/Solver.cs
+++ 2021/dotnet/day05/Solver.cs
@@ -29,7 +29,8 @@ namespace Solvers
             return $"({X},{Y})";
         }
 
-        public (int X, int Y) ToTuple() {
+        public (int X, int Y) ToTuple()
+        {
             return (X, Y);
         }
     }
@@ -46,76 +47,128 @@ namespace Solvers
 
             SolveTask1(TestData);
             SolveTask1(RealData);
-            // SolveTask2(TestData);
-            // SolveTask2(RealData);
+            SolveTask2(TestData);
+            SolveTask2(RealData);
         }
 
         public void SolveTask1(List<string> data)
         {
-            var segments = data.Where(IsNotDiagonal).Select(CreateLinesegment).ToList();
-            var lookup = new Dictionary<(int, int), int>();
-            var mapping = GetTuples(segments).Aggregate(lookup, (mapping, tuple) => {
-                if (mapping.ContainsKey(tuple)) {
-                    mapping[tuple]++;
-                } else {
-                    mapping[tuple] = 1;
-                }
-                return mapping;
-            });
+            var segments = data.Where(IsNotDiagonal).Select(CreateLinesegment);
+            var mapping = CreateMap(segments);
 
             Console.WriteLine(mapping.Values.Where(v => v > 1).Count());
         }
 
-        private IEnumerable<Coords> CreateLinesegment(string line) {
+        public void SolveTask2(List<string> data)
+        {
+            var segments = data.Select(CreateLinesegment);
+            var mapping = CreateMap(segments);
+            Console.WriteLine(mapping.Values.Where(v => v > 1).Count());
+        }
+
+        private IEnumerable<Coords> CreateLinesegment(string line)
+        {
             var pairs = line.Split(" -> ");
             var from = new Coords(pairs[0]);
             var to = new Coords(pairs[1]);
 
+            // diagonal line
+            if (from.X != to.X && from.Y != to.Y)
+            {
+                return GetDiagonalCoords(from, to);
+            }
+
             // horizontal line
-            if (from.X != to.X) {
-                if (from.X > to.X) {
-                    var tmp = from;
-                    from = to;
-                    to = tmp;
-                }
-                return Enumerable.Range(from.X, to.X - from.X + 1).Select(x => {
-                    return new Coords(x, from.Y);
-                });
+            if (from.X != to.X)
+            {
+                return GetHorizontalCoords(from, to);
             }
 
             // vertical line
-            if (from.Y != to.Y) {
-                if (from.Y > to.Y) {
-                    var tmp = from.Y;
-                    from.Y = to.Y;
-                    to.Y = tmp;
-                }
-                return Enumerable.Range(from.Y, to.Y - from.Y + 1).Select(y => {
-                    return new Coords(from.X, y);
+            if (from.Y != to.Y)
+            {
+                return GetVerticalCoords(from, to);
+            }
+
+            return new List<Coords> { };
+        }
+
+        private IEnumerable<Coords> GetHorizontalCoords(Coords from, Coords to)
+        {
+            (from, to) = from.X > to.X ? (to, from) : (from, to);
+            return Enumerable.Range(from.X, to.X - from.X + 1).Select(x =>
+            {
+                return new Coords(x, from.Y);
+            });
+        }
+
+        private IEnumerable<Coords> GetVerticalCoords(Coords from, Coords to)
+        {
+            (from, to) = from.Y > to.Y ? (to, from) : (from, to);
+            return Enumerable.Range(from.Y, to.Y - from.Y + 1).Select(y =>
+            {
+                return new Coords(from.X, y);
+            });
+        }
+
+        private IEnumerable<Coords> GetDiagonalCoords(Coords from, Coords to)
+        {
+            (from, to) = from.X > to.X ? (to, from) : (from, to);
+            var y = from.Y;
+            if (from.Y > to.Y)
+            {
+                return Enumerable.Range(from.X, to.X - from.X + 1).Select(x =>
+                {
+                    var coords = new Coords(x, y);
+                    y--;
+                    return coords;
+                });
+            }
+            else
+            {
+                return Enumerable.Range(from.X, to.X - from.X + 1).Select(x =>
+                {
+                    var coords = new Coords(x, y);
+                    y++;
+                    return coords;
                 });
             }
 
-            return new List<Coords>{};
         }
 
-        private IEnumerable<(int, int)> GetTuples(IEnumerable<IEnumerable<Coords>> segments) {
-            return segments.Aggregate(new List<(int, int)>(), (acc, segment) => {
+        private IEnumerable<(int, int)> GetTuples(IEnumerable<IEnumerable<Coords>> segments)
+        {
+            return segments.Aggregate(new List<(int, int)>(), (acc, segment) =>
+            {
                 acc.AddRange(segment.Select(c => c.ToTuple()));
                 return acc;
             });
         }
 
-        private bool IsNotDiagonal(string line) {
+        private Dictionary<(int, int), int> CreateMap(IEnumerable<IEnumerable<Coords>> segments)
+        {
+            var lookup = new Dictionary<(int, int), int>();
+            return GetTuples(segments).Aggregate(lookup, (mapping, tuple) =>
+            {
+                if (mapping.ContainsKey(tuple))
+                {
+                    mapping[tuple]++;
+                }
+                else
+                {
+                    mapping[tuple] = 1;
+                }
+                return mapping;
+            });
+        }
+
+        private bool IsNotDiagonal(string line)
+        {
             var pairs = line.Split(" -> ");
             var from = new Coords(pairs[0]);
             var to = new Coords(pairs[1]);
 
             return !(from.X != to.X && from.Y != to.Y);
-        }
-
-        public void SolveTask2(List<string> data)
-        {
-            var numbers = data.First().Split(',').Select(c => Int32.Parse(c)).ToList();
         }
     }
 }
